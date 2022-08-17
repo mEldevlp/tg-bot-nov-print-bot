99cd83c49752204172cffe1077f33171a3ed6cd1
Daniil Kondrashov <76869287+IndyDevlp@users.noreply.github.com>
2022-08-17 16:13:37 +0300

    upd

--- unit_tests.py
+++ unit_tests.py
@@ -1,20 +1,12 @@
-# import unittest
-# from other import *
-#
-#
-# class TestBotFunctional(unittest.TestCase):
-#     def test_user_exists(self):
-#         db = Database("datastorage.db")
-#         self.assertEqual(db.user_exists("2114464762"), True)
-#
-#     def test_user_exists2(self):
-#         db = Database("datastorage.db")
-#         self.assertEqual(db.user_exists("13372281488"), False)
+import unittest
+from other import *
 
 
-def last_digit(n1, n2):
-    return pow(n1, n2, 10)
+class TestBotFunctional(unittest.TestCase):
+    def test_user_exists(self):
+        db = Database("datastorage.db")
+        self.assertEqual(db.user_exists("2114464762"), True)
 
-
-if __name__ == '__main__':  # 7
-    print(last_digit(10, 15 ** 12))
+    def test_user_exists2(self):
+        db = Database("datastorage.db")
+        self.assertEqual(db.user_exists("13372281488"), False)
